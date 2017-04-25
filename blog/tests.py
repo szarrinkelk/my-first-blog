b51366472e94f1980ab9bf74bff048b4ae9dab0d
sepideh zarrinkelk <sepidehzarrinkelk@sepidehs-MacBook-Pro.local>
2017-04-25 14:01:21 +0200

    added util test and tweaked the view

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,7 @@ from blog.models import Comment, Post
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.test import TestCase
+from django.test import Client
 
 
 class CommentTest(TestCase):
@@ -10,12 +11,19 @@ class CommentTest(TestCase):
         self.user = User.objects.create(id=1)
         self.post = Post.objects.create(author=self.user)
         self.comment = Comment.objects.create(text="Hello", post=self.post)
+        self.client = Client()
 
     def test_comment_is_empty(self):
         with self.assertRaises(IntegrityError):
-            #comment = Comment.objects.create(text = "Hello")
             self.assertEqual(self.comment.text, "Hello")
 
     def test_approval(self):
         self.comment.approve()
         self.assertEqual(self.comment.approved_comment, True)
+
+    def test_context(self):
+        self.post = Post.objects.create(author=self.user)
+        self.post.publish()
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['posts']), 1)
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,6 @@ from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
