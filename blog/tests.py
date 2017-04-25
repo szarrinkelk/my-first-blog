d79a7256635b0fdd4a271f9481a39b41e53d1b99
sepideh zarrinkelk <sepidehzarrinkelk@sepidehs-MacBook-Pro.local>
2017-04-25 12:21:19 +0200

    added some tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
+from blog.models import Comment, Post
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+
+class CommentTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id=1)
+        self.post = Post.objects.create(author=self.user)
+        self.comment = Comment.objects.create(text="Hello", post=self.post)
+
+    def test_comment_is_empty(self):
+        with self.assertRaises(IntegrityError):
+            #comment = Comment.objects.create(text = "Hello")
+            self.assertEqual(self.comment.text, "Hello")
+
+    def test_approval(self):
+        self.comment.approve()
+        self.assertEqual(self.comment.approved_comment, True)
