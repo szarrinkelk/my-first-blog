482df798c0578ed3c456d92c2d84a6f805d62cac
sepideh zarrinkelk <sepidehzarrinkelk@sepidehs-MacBook-Pro.local>
2017-04-25 10:10:03 +0200

    Added comments and login reg

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 admin.site.register(Post) #To make our model visible on the admin page, we need to register the model
+admin.site.register(Comment)
+
